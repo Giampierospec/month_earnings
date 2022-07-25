92fca783a5a520b51f9557992a91652b9a985d77
Giampiero Specogna <giampi_12@hotmail.com>
2022-07-25 10:55:46 -0400

    fix: added a little bit of padding for loader

--- client/src/components/Loader/index.tsx
+++ client/src/components/Loader/index.tsx
@@ -2,7 +2,7 @@ import { Spinner } from '@chakra-ui/react'
 import React from 'react'
 
 const Loader: React.FC = () => {
-  return <Spinner size="xl" color="primary" />
+  return <Spinner size="xl" color="primary" py="15px" />
 }
 
 export default Loader
