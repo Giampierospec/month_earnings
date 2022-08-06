6870a9d5156cef9fca0c28984c2cc59bdb590798
Giampiero Specogna <giampi_12@hotmail.com>
2022-08-06 15:15:54 -0400

    feature: add heroku build

--- server/index.ts
+++ server/index.ts
@@ -7,6 +7,7 @@ import './src/db/connection'
 import { isLoggedIn } from './src/middleware/auth'
 import cookieParser from 'cookie-parser'
 import { startApolloServer } from './src/services/apolloServer'
+import path from 'path'
 
 dotenv.config()
 
@@ -15,13 +16,13 @@ const cookieSession = require('cookie-session')
 const app = express()
 
 const port = process.env.PORT
-
-app.use(
-	cookieSession({
-		keys: [process.env.SECRET],
-		maxAge: 24 * 60 * 60 * 1000,
-	})
-)
+if (process.env.NODE_ENV === 'product')
+	app.use(
+		cookieSession({
+			keys: [process.env.SECRET],
+			maxAge: 24 * 60 * 60 * 1000,
+		})
+	)
 app.use(cookieParser())
 app.use(cors())
 app.use(bodyParser.json())
@@ -29,6 +30,16 @@ app.use(isLoggedIn)
 ;(async () => {
 	await startApolloServer(app, TypeDefs, Resolvers)
 })()
+if (process.env.NODE_ENV === 'production') {
+	// Express will serve Production Assets
+	app.use(express.static(path.resolve(__dirname, '../client', 'build')))
+
+	//Express will serve up the index.html
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+	})
+}
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`)
 })
