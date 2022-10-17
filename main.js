9c4f35c05ae420e85f3c4a7b45b8da234e75425b
Giampiero Specogna <giampiero@analogrepublic.com>
2022-10-17 14:28:19 -0400

    feature: added electron wrapper

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+	const mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	})
+	mainWindow.loadURL('http://localhost:3000')
+}
+app.whenReady().then(() => {
+	createWindow()
+
+	app.on('activate', () => {
+		// On macOS it's common to re-create a window in the app when the
+		// dock icon is clicked and there are no other windows open.
+		if (BrowserWindow.getAllWindows().length === 0) createWindow()
+	})
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') app.quit()
+})
